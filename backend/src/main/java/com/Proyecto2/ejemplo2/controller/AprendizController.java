e04a2ad6c8ceb22732422e85017def502b647d59
maria <claroaleja@gmail.com>
2025-06-14 13:27:37 -0500

    Cambio controller

--- backend/src/main/java/com/Proyecto2/ejemplo2/controller/AprendizController.java
+++ backend/src/main/java/com/Proyecto2/ejemplo2/controller/AprendizController.java
@@ -26,6 +26,11 @@ public class AprendizController {
      return this.AprendizService.newAprendiz(aprendiz); 
  }
  
+ @GetMapping("/mostrar")
+ public Iterable<Aprendiz> getAll(){
+ return AprendizService.getAll();
+ }
+ 
  @PutMapping("/modificar")
  public Aprendiz updateAprendiz(@RequestBody Aprendiz aprendiz) {
  return this.AprendizService.modifyAprendiz(aprendiz);
